fc36f36d1328b253ce85d80ecac9a72d203b4aaa
Ben Lee <bendelonlee@gmail.com>
2019-03-25 16:38:19 -0600

    Adds empty gold stars to songs

--- src/html/song-html.js
+++ src/html/song-html.js
@@ -2,6 +2,7 @@ export const song = (track) => {
 
   return `
   <ul class="song" id="song-${track.id}">
+    <i class="far fa-star favorite-song"></i>
     <li class="song-name"> Name: ${track.track.track_name}</li>
     <li class="rating"> Rating: ${track.track.track_rating} </li>
   </ul>
