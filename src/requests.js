eb9293d814cd98712e27db752673bf1bacc9f7aa
Ben Lee <bendelonlee@gmail.com>
2019-03-25 14:02:23 -0600

    Rewrites Artist Search Request, splitting it into two requests

--- src/requests.js
+++ src/requests.js
@@ -1,17 +1,35 @@
 const $ = require("jquery");
 
-export const artistSearchRequest = (id) => {
+export const artistSearchRequest = (name, renderer) => {
+  const tracksByArtist = (artist) => {
+    let id = artist.artist_id;
+    $.ajax({
+      url: `https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1&apikey=60c354a43ae452685248b841bfa271e2&f_artist_id=${id}`,
+      data: {
+        format: 'jsonp',
+        callback: 'jsonp_callback'
+      },
+      dataType: 'jsonp',
+      jsonpCallback: 'jsonp_callback',
+      success: function(result){
+        renderer(artist, result.message.body.track_list)
+      }
+    });
+  }
 
-  $.ajax({url: "https://api.musixmatch.com/ws/1.1/track.search?format=jsonp&callback=callback&quorum_factor=1&apikey=60c354a43ae452685248b841bfa271e2&f_artist_id=118",
-    data: {
-      format: 'jsonp',
-      callback: 'jsonp_callback'
-    },
-    dataType: 'jsonp',
-    jsonpCallback: 'jsonp_callback',
-    success: function(result){
-      debugger;
-      $("body").html(result);
-    }
-  });
+  const artistByName = (name, nextRequest) => {
+    return $.ajax({url: `https://api.musixmatch.com/ws/1.1/artist.search?format=jsonp&callback=callback&quorum_factor=1&apikey=60c354a43ae452685248b841bfa271e2&q_artist=${name}`,
+      data: {
+        format: 'jsonp',
+        callback: 'jsonp_callback'
+      },
+      dataType: 'jsonp',
+      jsonpCallback: 'jsonp_callback',
+      success: function(result){
+        let artist = result.message.body.artist_list[0].artist;
+        nextRequest(artist);
+      }
+    });
+  }
+  artistByName(name, tracksByArtist);
 }
