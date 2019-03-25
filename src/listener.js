f1538f3f427e3f2f43fe2eccaf4e55bfecb9c43d
Ben Lee <bendelonlee@gmail.com>
2019-03-25 15:22:16 -0600

    Adds enter keyup listener to listenForSearch

--- src/listener.js
+++ src/listener.js
@@ -8,9 +8,17 @@ export class Listener {
   }
 
   listenForSearch() {
-    $('#search-button').click(function () {
+    const search = () => {
       let artistName = $('#search-input').val();
       new ArtistSearch(artistName).fetch_and_render();
+    }
+    $('#search-button').click(function () {
+      search();
     });
+    $('#search-input').keyup(function (key) {
+    if (key.which == 13) {
+      search();
+    }
+  });
   }
 }
