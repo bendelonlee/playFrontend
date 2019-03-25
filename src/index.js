b372c2a360c4c89b987abd732e47a797b78b56b2
Ben Lee <bendelonlee@gmail.com>
2019-03-25 15:34:58 -0600

    Adds space under nav

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './styles.css';
 import { Listener } from './listener.js';
 
 $(document).ready(function() {
+  $('#under-nav').height($('nav').height() + 20);
   Listener.prototype.start();
 });
